008fe0a70cb43385cdc4a144a678476e54b63f0f
SHYAM SUNDHAR G <83800362+shyamgsundhar@users.noreply.github.com>
2025-03-23 21:38:33 +0530

    Create swap_most_Superb_trick.java

---
+++ swap_most_Superb_trick.java
@@ -0,0 +1,10 @@
+class Main {
+	public static void main(String s[]) {
+		int a=15,b=30;
+		System.out.println("The A and B is \t"+ a+"\t"+b);
+		a=a;
+		b=(a+b)-(a=b);
+		System.out.println();
+		System.out.println("The A and B is \t"+ a+"\t"+b);
+	}
+}
