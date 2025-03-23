ed861cf5ee05c4ce56797946d5962c8f14d8a2f9
SHYAM SUNDHAR G <83800362+shyamgsundhar@users.noreply.github.com>
2025-03-23 21:20:17 +0530

    Create subract_using_XOR.java

---
+++ subract_using_XOR.java
@@ -0,0 +1,12 @@
+class Main {
+	public static void main(String s[]) {
+		int a=15,b=30;
+		while(b!=0){
+		    int borrow = (~a&b);
+		    a=a^b;
+		    b=borrow<<1;
+		}
+
+		System.out.println("The Answer is \t"+ a);
+	}
+}
